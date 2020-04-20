3aa633e865542d8ec1cbf0e93f585a4ac0bb546a
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2020-04-20 10:58:41 +0200

    place object in ar

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    private PlacementIndicator placementIndicator;
+
+    private void Start()
+    {
+        placementIndicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            var obj = Instantiate(objectToSpawn, placementIndicator.transform.position,
+                placementIndicator.transform.rotation);
+        }
+    }
+}
