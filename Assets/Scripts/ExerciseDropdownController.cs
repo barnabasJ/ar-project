aa7871fae004b05c054c5b75ea3ebddf1e390b89
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2020-05-02 13:51:39 +0200

    working exercise dropdown

--- Assets/Scripts/ExerciseDropdownController.cs
+++ Assets/Scripts/ExerciseDropdownController.cs
@@ -1,14 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class ExerciseDropdownController : MonoBehaviour
 {
     public List<GameObject> exercises = new List<GameObject>();
-    // Start is called before the first frame update
     private GameObject exerciseDropdown;
     private GameObject activeExercise;
     void Start() 
@@ -16,7 +12,6 @@ public class ExerciseDropdownController : MonoBehaviour
         exerciseDropdown = GameObject.FindWithTag("ExerciseDropdown");
         var dropDown = exerciseDropdown.GetComponentInChildren<TMP_Dropdown>();
         dropDown.onValueChanged.AddListener(onDropDownChanged);
-        Debug.Log("hello");
     }
 
     void onDropDownChanged(int index)
@@ -31,13 +26,6 @@ public class ExerciseDropdownController : MonoBehaviour
         {
             activeExercise = Instantiate(exercises[index - 1]);
         }
-        
-    }
-    
-
-    // Update is called once per frame
-    void Update()
-    {
         
     }
 }
